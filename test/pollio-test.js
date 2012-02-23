5aea5ba39575404f4cab2e458e51d67325d464f9
bcoe <bencoe@gmail.com>
2012-02-23 11:43:55 -0800

    Making sure default variables are properly defined.

--- test/pollio-test.js
+++ test/pollio-test.js
@@ -1,10 +1,16 @@
 var equal = require('assert').equal,
 	puts = require('util').puts,
 	PollIO = require('../lib/pollio').PollIO;
-	
+
+global.jQuery = {
+	ajax: function() {}
+};
+
 exports.tests = {
-	'should be an application': function(finished, prefix) {
-		var pollio = new PollIO();
-		equal(true, false, prefix + ' true was not false.');
+	'should allow the first parameter to be either params or the ajax function': function(finished, prefix) {
+		var pollio = new PollIO({pollLoopFrequency: 3000});
+		equal(3000, pollio.pollLoopFrequency, prefix + ' true was not false.');
+		equal('function', typeof(pollio.ajax), prefix + ' ajax function was not defined.');
+		finished();
 	}
 };
